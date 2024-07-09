e4deaa0a07e351bf47b012d5227483f6a2152a9d
sturm1619 <carlosrubio1921@gmail.com>
2024-07-09 10:26:19 -0600

    just some clean up

--- frontend/interview.py
+++ frontend/interview.py
@@ -20,13 +20,20 @@ if "data" not in st.session_state:
             st.write(str(response.reason) + " " + str(response.status_code))
 else:
     with st.form(key="interviewForm"):
+
         st.write(f"Name: {st.session_state["data"]["name"]}")
         st.write(f"UVU ID: {st.session_state["data"]["uvuid"]}")
         q1 = st.checkbox("Can code")
         q2 = st.number_input("Enjoyment", min_value=1, max_value=10, value=5)
         q3 = st.number_input("Social", min_value=1, max_value=10, value=5)
+
         if st.form_submit_button("Submit"):
-            interview_data = json.dumps(dict(fkuser=st.session_state["data"]["pid"], cancode=q1, enjoyment=q2, social=q3))
+            interview_data = json.dumps(
+                dict(
+                    fkuser=st.session_state["data"]["pid"], 
+                    cancode=q1, enjoyment=q2, social=q3
+                )
+            )
             # I doubt request.post works. Blame on 
             post = requests.post(backend + "interviewFinish",
                                  json=interview_data, headers={"Content-Type": "application/json"})
@@ -37,14 +44,14 @@ else:
                 st.write(post.status_code)
                 st.write("There was an error in the submission.")
 
-st.title("2nd Portion")
+#st.title("2nd Portion")
 
-second_id = st.text_input("Enter UVU ID for 2nd Portion:")
-if st.button("Submit 2nd Portion"):
-    response = requests.post(
-        backend + "interviewStart", json={"uvuid": str(second_id)}, headers={"Content-Type": "application/json"})
-    if response.status_code == 200:
-        second_data = response.json()
-        st.json(second_data)
-    else:
-        st.write(str(response.reason) + " " + str(response.status_code))
+# second_id = st.text_input("Enter UVU ID for 2nd Portion:")
+# if st.button("Submit 2nd Portion"):
+#     response = requests.post(
+#         backend + "interviewStart", json={"uvuid": str(second_id)}, headers={"Content-Type": "application/json"})
+#     if response.status_code == 200:
+#         second_data = response.json()
+#         st.json(second_data)
+#     else:
+#         st.write(str(response.reason) + " " + str(response.status_code))
