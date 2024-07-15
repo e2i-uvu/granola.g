9e359eff7630e7d5fb3619a7cfc8ce0693f67193
Spencer <sthompson.dev@outlook.com>
2024-07-15 19:43:08 +0000

    BIG changes, added a bunch of pages

---
+++ frontend/README.md
@@ -0,0 +1,16 @@
+# How to
+
+The `app.py` is the *parent* of the rest of the streamlit pages.
+
+It acts as the entrypoint or router to send traffic to all other pages.
+
+There is a bunch of code that defines:
+
+* Styles
+* Titles
+* Menus
+* Other stuff for whole app
+
+## New Pages
+
+To add or edit pages, just look at `st.navigation` inside the `app.py`
--- frontend/app.py
+++ frontend/app.py
@@ -5,6 +5,8 @@ Entrypoint for the streamlit frontend
 import streamlit as st
 import sys
 
+VERSION = 0.1
+
 try:
     if sys.argv[1] == "dev":
         st.session_state["dev"] = True
@@ -15,31 +17,62 @@ except IndexError:
     st.session_state["dev"] = False
 
 
+def style(filename: str = "./styles/main.css"):
+    """Hide default style. Additionally add custom styles"""
+    with open(filename, "r") as f:
+        css = f.read()
+    return f"<style>{css}</style>"
+
+
+st.set_page_config(
+    page_title="E2i",
+    page_icon=":material/school:",
+    layout="wide",
+    initial_sidebar_state="expanded",
+    menu_items={
+        # TODO: Guts take a look at this
+        # ./styles/main.css hides this,
+        # but maybe we should add these
+        "Get Help": "https://www.extremelycoolapp.com/help",
+        "Report a bug": "https://www.extremelycoolapp.com/bug",
+        "About": "# This is a header. This is an *extremely* cool app!",
+    },
+)
+
 # any UI elements in this file will be rendered
 # on every page of the streamlit app
 
 # st.title("Team Building")
 
+
+st.markdown(style(), unsafe_allow_html=True)
+
+
 st.logo(
     image="./static/innovation-academy-logo-side-green.png",
     link="https://www.uvu.edu/innovation/e2i/",
-    icon_image="./static/uvu-logo-green.png",
+    icon_image="./static/uvu-logo-cropped-green.png",
 )
 
 # can add pages here
 pages = {
+    "Info": [
+        st.Page("about.py", title="About", icon=":material/info:", default=False),
+        st.Page("guide.py", title="How to", icon=":material/help:"),
+    ],
     "Teams": [
         st.Page("interview.py", title="Interview", icon=":material/person_add:"),
         st.Page("employees.py", title="Employees", icon=":material/groups:"),
         st.Page(
             "teams.py",
             title="Team Building",
-            default=False,
             icon=":material/reduce_capacity:",
         ),
         st.Page("chat.py", title="AI Chat", icon=":material/chat:"),
     ],
 }
+
+st.sidebar.caption(f"Version: :green-background[{VERSION}]")
 
 pg = st.navigation(pages, position="sidebar")
 pg.run()
