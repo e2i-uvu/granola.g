f88eeb756c7ed6dba451ac8da5b9433ae963eeaf
Guts Wright <gutsw20@gmail.com>
2024-07-16 15:52:26 +0000

    Add drop down employees

--- frontend/employees.py
+++ frontend/employees.py
@@ -3,5 +3,19 @@ import streamlit as st
 
 st.title("Employees")
 
+option = st.selectbox('Select one',
+    ('Select an option', 'hire', 'status', 'fire')
+)
+
+# st.write(f'You selected: {option}')
+if option != 'Select an option':
+    if option == 'hire':
+        st.write('You are hiring this person')
+    elif option == 'status':
+        st.write('Here is the status')
+    elif option == 'fire':
+        st.write('Congratulations! You have been fired!')
+
+
 # TODO: Guts this will be the status page for current employees
 # Including hiring and firing as we talked about today
