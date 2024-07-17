1fa11146399844ab203bc1f60528c5de05e1a77d
sturm1619 <carlosrubio1921@gmail.com>
2024-07-16 19:09:53 -0600

    weightConfig now displays sliders and stores configuration

--- frontend/weightConfig.py
+++ frontend/weightConfig.py
@@ -1,32 +1,59 @@
-#import streamlit as st
+import streamlit as st
 import json
 import os
-import logging
+#import logging
+import numpy as np
 from copy import deepcopy
+
+# TODO: Add logging functionality?
 
 if __name__ == "__main__":
     #configFile = "./frontend/.weightConfig.json"
-    configFilePath = ".weightConfig.json"
-    defaultConfig = str(dict(enjoyment = 3.0, social = 2.0))
+    # TODO:  Maybe make this configFilePath a env?
+    configFilePath: str = ".weightConfig.json"
+    defaultConfig: str = '{"Enjoyment" : 3.0, "Social" : 2.0}'
 
-    """Confirm existence of configuration file."""
+    # """Import saved configuration."""
     try:
         if os.stat(configFilePath).st_size == 0:
-            # print("File is empty")
-            with open(configFile, "w") as configFile:
+            with open(configFilePath, "w") as configFile:
                 configFile.write(defaultConfig)
+        else:
+            with open(configFilePath, "r") as configFile:
+                _ = json.load(configFile)
+    except JSONDecodeError:
+        st.write("Configuration file is corrupted. Loading Default Configuration.")
 
     except FileNotFoundError:
-        # print("File not found error!")
+        st.write("Configuration file not found.")
         with open(configFile, "w") as configFile:
             configFile.write(defaultConfig)
 
-    """Import saved configuration."""
-    with open(configFilePath, "r") as configFile:
-        ORIGINAL_CONFIG: dict[str, float] = json.loads(configFile)
-        weightConfig = deepcopy(ORIGINAL_CONFIG)
-
-    """Save new configuration."""
+    finally:
+        with open(configFilePath, "r") as configFile:
+                ORIGINAL_CONFIG: dict[str, float] = json.load(configFile)
+                st.session_state['weightConfig'] = deepcopy(ORIGINAL_CONFIG)
 
 
-    
+    # Display Configuration
+    st.title("Weight Configuration")
+
+    for key, value in st.session_state['weightConfig'].items():
+        st.select_slider(label = key.title(),
+                         options = np.arange(start = 1, stop = 11, step = 1),
+                         value = value,
+                         key = key)
+
+    #"""Save new configuration."""
+    if st.button("Save Configuration"):
+        serializableConfigState = {}
+        for key in st.session_state['weightConfig']:
+            st.session_state['weightConfig'][key] = st.session_state[key]
+            serializableConfigState[key] = int(st.session_state[key])
+
+        with open(configFilePath, "w") as configFile:
+            json.dump(obj = serializableConfigState, fp = configFile)
+
+    #"""Restore Configuration"""
+    if st.button("Restore Configuration"):
+        st.session_state['weightConfig'] = deepcopy(ORIGINAL_CONFIG)
