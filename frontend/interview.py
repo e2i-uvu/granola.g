5db805defcc57c2bbaf4913c62de79adbb9005aa
Guts Wright <gutsw20@gmail.com>
2024-07-23 18:25:07 +0000

    Submit button double press fix. Go to interview.py main after submitting.

--- frontend/interview.py
+++ frontend/interview.py
@@ -3,6 +3,12 @@ import requests
 from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 import os
+import time
+
+# when a UVUID is pressed the first time have it go to the next screen
+# allow functionality so that you can hit the enter button which will also trigger the submit button
+# when all of the enjoyment and other info has been entered correctly, the submit button will return the user to the original screen
+# (this has something to do with the session state)
 
 load_dotenv()
 backend = os.getenv("BACKEND")
@@ -11,15 +17,25 @@ password = os.getenv("PASSWORD")
 
 st.title("Interview App")
 
+
+if "IDs_entered" not in st.session_state:
+    st.session_state.IDs_entered = []
+
 if "data" not in st.session_state:
+    if st.session_state.IDs_entered != []:
+        st.write("Interview form submitted succesfully.")
+        st.write(st.session_state.IDs_entered)
     id = st.text_input("Enter your UVU ID:")
     if st.button("Submit"):
         response = requests.post(
             backend + "interviewStart", json={"uvuid": str(id)}, headers={"Content-Type": "application/json"})
         if response.status_code == 200:
             st.session_state["data"] = response.json()
+            time.sleep(0.01) # this allows time for the sessionstate to update before making the page reload
+            st.rerun() # reload the current page (these two line fix the submit button having to be pushed twice)
         else:
             st.write(str(response.reason) + " " + str(response.status_code))
+
 else:
     with st.form(key="interviewForm"):
 
@@ -39,7 +55,10 @@ else:
                 json=interview_data, headers={"Content-Type": "application/json"})
             if post.status_code == 200:
                 st.write(post.status_code)
-                st.write("Interview form submitted succesfully.")
+                st.session_state.IDs_entered.append(int(st.session_state["data"]["uvuid"]))
+                del st.session_state["data"]
+                time.sleep(0.01)
+                st.rerun()
             else:
                 st.write(post.status_code)
                 st.write("There was an error in the submission.")
