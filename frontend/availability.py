608242f9554e21390836d4aac3342b10535c7295
Guts <gutsw20@gmail.com>
2024-08-02 07:26:40 +0000

    Potential idea for inputing availability

--- frontend/availability.py
+++ frontend/availability.py
@@ -1,248 +1,57 @@
 import streamlit as st
-from streamlit_calendar import calendar
+import pandas as pd
 
-mode = st.selectbox(
-    "Calendar Mode:",
-    (
-        "daygrid",
-        "timegrid",
-        "timeline",
-        "resource-daygrid",
-        "resource-timegrid",
-        "resource-timeline",
-        "list",
-        "multimonth",
-    ),
-)
+st.title("My Availability")
 
-events = [
-    {
-        "title": "Event 1",
-        "color": "#FF6C6C",
-        "start": "2023-07-03",
-        "end": "2023-07-05",
-        "resourceId": "a",
-    },
-    {
-        "title": "Event 2",
-        "color": "#FFBD45",
-        "start": "2023-07-01",
-        "end": "2023-07-10",
-        "resourceId": "b",
-    },
-    {
-        "title": "Event 3",
-        "color": "#FF4B4B",
-        "start": "2023-07-20",
-        "end": "2023-07-20",
-        "resourceId": "c",
-    },
-    {
-        "title": "Event 4",
-        "color": "#FF6C6C",
-        "start": "2023-07-23",
-        "end": "2023-07-25",
-        "resourceId": "d",
-    },
-    {
-        "title": "Event 5",
-        "color": "#FFBD45",
-        "start": "2023-07-29",
-        "end": "2023-07-30",
-        "resourceId": "e",
-    },
-    {
-        "title": "Event 6",
-        "color": "#FF4B4B",
-        "start": "2023-07-28",
-        "end": "2023-07-20",
-        "resourceId": "f",
-    },
-    {
-        "title": "Event 7",
-        "color": "#FF4B4B",
-        "start": "2023-07-01T08:30:00",
-        "end": "2023-07-01T10:30:00",
-        "resourceId": "a",
-    },
-    {
-        "title": "Event 8",
-        "color": "#3D9DF3",
-        "start": "2023-07-01T07:30:00",
-        "end": "2023-07-01T10:30:00",
-        "resourceId": "b",
-    },
-    {
-        "title": "Event 9",
-        "color": "#3DD56D",
-        "start": "2023-07-02T10:40:00",
-        "end": "2023-07-02T12:30:00",
-        "resourceId": "c",
-    },
-    {
-        "title": "Event 10",
-        "color": "#FF4B4B",
-        "start": "2023-07-15T08:30:00",
-        "end": "2023-07-15T10:30:00",
-        "resourceId": "d",
-    },
-    {
-        "title": "Event 11",
-        "color": "#3DD56D",
-        "start": "2023-07-15T07:30:00",
-        "end": "2023-07-15T10:30:00",
-        "resourceId": "e",
-    },
-    {
-        "title": "Event 12",
-        "color": "#3D9DF3",
-        "start": "2023-07-21T10:40:00",
-        "end": "2023-07-21T12:30:00",
-        "resourceId": "f",
-    },
-    {
-        "title": "Event 13",
-        "color": "#FF4B4B",
-        "start": "2023-07-17T08:30:00",
-        "end": "2023-07-17T10:30:00",
-        "resourceId": "a",
-    },
-    {
-        "title": "Event 14",
-        "color": "#3D9DF3",
-        "start": "2023-07-17T09:30:00",
-        "end": "2023-07-17T11:30:00",
-        "resourceId": "b",
-    },
-    {
-        "title": "Event 15",
-        "color": "#3DD56D",
-        "start": "2023-07-17T10:30:00",
-        "end": "2023-07-17T12:30:00",
-        "resourceId": "c",
-    },
-    {
-        "title": "Event 16",
-        "color": "#FF6C6C",
-        "start": "2023-07-17T13:30:00",
-        "end": "2023-07-17T14:30:00",
-        "resourceId": "d",
-    },
-    {
-        "title": "Event 17",
-        "color": "#FFBD45",
-        "start": "2023-07-17T15:30:00",
-        "end": "2023-07-17T16:30:00",
-        "resourceId": "e",
-    },
-]
-calendar_resources = [
-    {"id": "a", "building": "Building A", "title": "Room A"},
-    {"id": "b", "building": "Building A", "title": "Room B"},
-    {"id": "c", "building": "Building B", "title": "Room C"},
-    {"id": "d", "building": "Building B", "title": "Room D"},
-    {"id": "e", "building": "Building C", "title": "Room E"},
-    {"id": "f", "building": "Building C", "title": "Room F"},
-]
+time_slots = ['07:00 AM', '07:30 AM', '08:00 AM', '08:30 AM', 
+              '09:00 AM', '09:30 AM', '10:00 AM', '10:30 AM', '11:00 AM', 
+              '11:30 AM', '12:00 PM', '12:30 PM', '01:00 PM', '01:30 PM',
+              '02:00 PM', '02:30 PM', '03:00 PM', '03:30 PM', '04:00 PM',
+              '04:30 PM', '05:00 PM', '05:30 PM', '06:00 PM', '06:30 PM',
+              '07:00 PM', '07:30 PM', '08:00 PM', '08:30 PM', '09:00 PM',
+              '09:30 PM', '10:00 PM']
 
-calendar_options = {
-    "editable": "true",
-    "navLinks": "true",
-    "resources": calendar_resources,
-    "selectable": "true",
+data = {
+    "Mon": [False] * len(time_slots),
+    "Tue": [False] * len(time_slots),
+    "Wed": [False] * len(time_slots),
+    "Thu": [False] * len(time_slots),
+    "Fri": [False] * len(time_slots),
+    "Sat": [False] * len(time_slots),
+    "Sun": [False] * len(time_slots),
 }
+df = pd.DataFrame(data, index=time_slots)
 
-if "resource" in mode:
-    if mode == "resource-daygrid":
-        calendar_options = {
-            **calendar_options,
-            "initialDate": "2023-07-01",
-            "initialView": "resourceDayGridDay",
-            "resourceGroupField": "building",
-        }
-    elif mode == "resource-timeline":
-        calendar_options = {
-            **calendar_options,
-            "headerToolbar": {
-                "left": "today prev,next",
-                "center": "title",
-                "right": "resourceTimelineDay,resourceTimelineWeek,resourceTimelineMonth",
-            },
-            "initialDate": "2023-07-01",
-            "initialView": "resourceTimelineDay",
-            "resourceGroupField": "building",
-        }
-    elif mode == "resource-timegrid":
-        calendar_options = {
-            **calendar_options,
-            "initialDate": "2023-07-01",
-            "initialView": "resourceTimeGridDay",
-            "resourceGroupField": "building",
-        }
-else:
-    if mode == "daygrid":
-        calendar_options = {
-            **calendar_options,
-            "headerToolbar": {
-                "left": "today prev,next",
-                "center": "title",
-                "right": "dayGridDay,dayGridWeek,dayGridMonth",
-            },
-            "initialDate": "2023-07-01",
-            "initialView": "dayGridMonth",
-        }
-    elif mode == "timegrid":
-        calendar_options = {
-            **calendar_options,
-            "initialView": "timeGridWeek",
-        }
-    elif mode == "timeline":
-        calendar_options = {
-            **calendar_options,
-            "headerToolbar": {
-                "left": "today prev,next",
-                "center": "title",
-                "right": "timelineDay,timelineWeek,timelineMonth",
-            },
-            "initialDate": "2023-07-01",
-            "initialView": "timelineMonth",
-        }
-    elif mode == "list":
-        calendar_options = {
-            **calendar_options,
-            "initialDate": "2023-07-01",
-            "initialView": "listMonth",
-        }
-    elif mode == "multimonth":
-        calendar_options = {
-            **calendar_options,
-            "initialView": "multiMonthYear",
-        }
+col0, col1, col2, col3 = st.columns([.55, .2, .2, .15], vertical_alignment='bottom')
 
-state = calendar(
-    events=st.session_state.get("events", events),
-    options=calendar_options,
-    custom_css="""
-    .fc-event-past {
-        opacity: 0.8;
-    }
-    .fc-event-time {
-        font-style: italic;
-    }
-    .fc-event-title {
-        font-weight: 700;
-    }
-    .fc-toolbar-title {
-        font-size: 2rem;
-    }
-    """,
-    key=mode,
-)
+with col0:
+    options = st.multiselect(
+        "Select Days to apply these times",
+        ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    )
 
-if state.get("eventsSet") is not None:
-    st.session_state["events"] = state["eventsSet"]
+with col1:
+    start_time = st.selectbox(
+        "Start time",
+        time_slots
+    )
 
-st.write(state)
+with col2:
+    end_time = st.selectbox(
+        "End time",
+        time_slots
+    ) 
 
-st.markdown("## API reference")
-st.help(calendar)
+with col3:
+    if st.button("Save"):
+        start_idx = time_slots.index(start_time)
+        end_idx = time_slots.index(end_time)
+        if start_idx > end_idx:
+            st.error("Start time must be before end time.")
+        else:
+            for day in options:
+                df.loc[start_time:end_time, day] = True
+
+edited_df = st.data_editor(df, height=1125, width=1000)
+
+df.update(edited_df)
