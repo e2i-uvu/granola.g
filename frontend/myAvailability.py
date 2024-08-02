cc90469c6282cfdde9d16c2e76cc7972799f8c70
Guts <gutsw20@gmail.com>
2024-08-02 00:56:13 +0000

    Add frontend myAvailability.py

--- frontend/myAvailability.py
+++ frontend/myAvailability.py
@@ -1,3 +1,24 @@
+
 import streamlit as st
+import pandas as pd
+import numpy as np
 
 st.title("My Availability")
+
+time_slots = pd.date_range("07:00", "22:00", freq="30min").strftime("%I:%M %p")
+
+
+data = {
+    "Monday": [False] * len(time_slots),
+    "Tuesday": [False] * len(time_slots),
+    "Wednesday": [False] * len(time_slots),
+    "Thursday": [False] * len(time_slots),
+    "Friday": [False] * len(time_slots),
+    "Saturday": [False] * len(time_slots),
+    "Sunday": [False] * len(time_slots)
+}
+
+df = pd.DataFrame(data, index=time_slots)
+
+st.data_editor(df, height=1125, width=1000)
+
