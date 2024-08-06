6e1da3fa992a59c774c4c56f1f1b621804a36f4d
sturm1619 <carlosrubio1921@gmail.com>
2024-08-05 19:49:16 -0600

    Renamed modules

--- frontend/test/employee_status.py
+++ frontend/test/employee_status.py
@@ -15,25 +15,42 @@ backend, username, password, _ = testutils.set_basic_state()
 
 #module_logger = logging.getLogger("employee_statusHandler_Test")
 
-def employee_status(url: str = "status"):
+def get_employee_status(url: str = "status"):
     request = requests.get(
             backend + url,
             auth = HTTPBasicAuth(username,password)
     )
     return request.status_code
 
-def test_employee_status(url: str = "status"):
+def post_employee_status(url: str, data: dict[str,str]) -> int:
+    response = requests.post(
+        backend + url,
+        auth = HTTPBasicAuth(username, password),
+        json = data
+    )
+    return response.status_code
+
+def test_get(url: str = "status") -> None:
     # NOTE: Should I test the status_code or also the structure of the contents?
-    module_logger = logging.getLogger(f"{url.title()}_Test")
-    if employee_status(url) == 200:
+    module_logger = logging.getLogger(f"get_{url.title()}_Test")
+    if get_employee_status(url) == 200:
         module_logger.info(f"succesfully returns data.")
     else:
         module_logger.error(f"fails to return data.")
 
+
+def test_post(url: str, data: dict[str,str]) -> None:
+    module_logger = logging.getLogger(f"post_{url.title()}_Test")
+    if post_employee_status(url) == 200:
+        module_logger.info("successfully returns data.")
+    else:
+        module_logger.error(f"fails to return data.")
+
+
 def run_tests():
-    test_employee_status("status")
-    test_employee_status("hire")
-    test_employee_status("fire")
+    test_get("status")
+    test_get("hire")
+    test_get("fire")
 
 
 if __name__ == "__main__":
