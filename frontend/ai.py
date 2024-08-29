0201c2d65bc2aabe69a66ee93dbdd7de8dfb833c
Guts Wright <gutsw20@gmail.com>
2024-08-29 18:56:37 +0000

    better working version

--- frontend/ai.py
+++ frontend/ai.py
@@ -1,30 +1,38 @@
 import streamlit as st
-import json
 import pandas as pd
 
+def display_data_editor(data):
+
+    df = pd.DataFrame(data)
+
+    if 'Remove' not in df.columns:
+        df.insert(0, 'Remove', False)
+
+    return st.data_editor(df.to_dict('records'))
 
 def display_team(team_json):
     if 'team_data' not in st.session_state:
         st.session_state['team_data'] = team_json
-    # st.json(team_json)
 
-    # st.data_editor(team_json)
+    initial_data = pd.DataFrame(st.session_state['team_data'])
+    if 'Remove' not in initial_data.columns:
+        initial_data.insert(0, 'Remove', False)
 
-    df = pd.DataFrame(st.session_state['team_data'])
+    st.session_state['team_data'] = initial_data.to_dict('records')
 
-    if 'Remove' not in df.columns:
-        df.insert(0, 'Remove', False)
-    # df.insert(1, 'Image2', '<img src="assets/minus_icon.jpg"')
-    edited_df = st.data_editor(df.to_dict('records'))
+    data_editor_placeholder = st.empty() # this holds the data_editor container
+
+    if st.session_state['team_data']:
+        edited_df = data_editor_placeholder.data_editor(st.session_state['team_data'])
+    else:
+        data_editor_placeholder.write("Team is empty")
 
     if st.button('Save'):
-
         st.session_state['team_data'] = edited_df
 
         st.session_state['team_data'] = [row for row in st.session_state['team_data'] if not row['Remove']]
 
-        st.success('Changes have been saved!')
-
-        df = pd.DataFrame(st.session_state['team_data'])
-        st.data_editor(df.to_dict('records'))
-        st.rerun()
+        if st.session_state['team_data']:
+            data_editor_placeholder.data_editor(st.session_state['team_data'])
+        else:
+            data_editor_placeholder.write("Team is empty")
