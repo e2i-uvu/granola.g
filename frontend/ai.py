d1164ba1385333d9f18dace7c45da9b8867ae598
Guts <gutsw20@gmail.com>
2024-08-29 15:35:09 +0000

    Tried removing from dataframe

--- frontend/ai.py
+++ frontend/ai.py
@@ -1,51 +1,14 @@
-# #this is where my function goes that returns the 
-
-# import streamlit as st
-
-# import toml
-
-# USERS = "./.streamlit/users.toml"
-
-# # TODO: So after reading the docs, we can't update `secrets.toml`
-# # and work with the updates without restarting the app.
-# # So I am thinking we might as well just change the file
-
-# user_data = toml.load(USERS)
-
-# st.header("Users")
-
-# with st.form("verification", border=False):
-#     modified_user_data = st.data_editor(
-#         user_data,
-#         column_config={
-#             "id": st.column_config.NumberColumn("UVID", disabled=True, format="%i"),
-#             "verified": st.column_config.CheckboxColumn(
-#                 "Verified?",
-#                 help="Select to allow access",
-#                 default=False,
-#             ),
-#         },
-#         # disabled
-#         use_container_width=True,
-#         hide_index=True,
-#     )
-#     submitted = st.form_submit_button(
-#         "Process Changes", type="primary", use_container_width=True
-#     )
-#     if submitted:
-#         with open(USERS, "w") as f:
-#             toml.dump(modified_user_data, f)
-
-#         st.toast("success")
-
 import streamlit as st
 import json
+import pandas as pd
+
 
 def display_team(team_json):
-    # st.markdown(
-    #     f"{team_json}This now should appear on screen"
-    # )
     st.json(team_json)
-    st.data_editor(team_json)
 
-    print(team_json)
+    # st.data_editor(team_json)
+
+    df = pd.DataFrame(team_json)
+    df.insert(0, 'Remove', False)
+    df.insert(1, 'Image', '<img src="assets/minus_icon.jpg"')
+    st.data_editor(df.to_dict('records'))
