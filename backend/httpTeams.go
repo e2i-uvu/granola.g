7241d4164aaced28804398d82607e3348cdaafe1
HenryH054 <henryphutchinson@gmail.com>
2024-09-10 09:27:49 -0600

    adds the backend for teambuilding, still doesn't allow for saving a team. resolves #39

--- backend/httpTeams.go
+++ backend/httpTeams.go
@@ -1,8 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 )
+
+type AOI struct {
+	Aoi    string `json:"employee"`
+	Amount int    `json:"amount"`
+}
+
+type Project struct {
+	ProjectName    string `json:"project_name"`
+	Type           string `json:"project_type"`
+	AOIs           []AOI  `json:"employees"`
+	TotalEmployees int    `json:"total_employees"`
+}
 
 func TeamsHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" && r.Header.Get("Content-Type") == "application/json" {
@@ -19,6 +32,29 @@ func TeamsHandler(w http.ResponseWriter, r *http.Request) {
 		// saves project to projects and then adds the employee ids and project ids to many to many table
 	}
 	if r.Method == "GET" && r.Header.Get("Content-Type") == "application/json" {
-		// BuildTeams()
+		var project Project
+		decoder := json.NewDecoder(r.Body)
+		err := decoder.Decode(&project)
+		if err != nil {
+			InfoLogger.Println("Error decoding JSON:", err)
+			return
+		}
+
+		team, err := BuildTeams(project)
+		if err != nil {
+			InfoLogger.Println("Couldn't build teams")
+			http.Error(w, "Invalid input", http.StatusBadRequest)
+			return
+		}
+
+		response, err := json.Marshal(team)
+		if err != nil {
+			InfoLogger.Println("Couldn't marshal data for hires")
+			http.Error(w, "Invalid input", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(response)
 	}
 }
--- backend/teambuilding.go
+++ backend/teambuilding.go
@@ -18,11 +18,11 @@ import (
 //	aoi TEXT,
 //	social Decimal(2,0),
 //	status INTEGER)`)
-func BuildTeams(project BuildProject) ([]Employee, error) {
+func BuildTeams(project Project) ([]Employee, error) {
 	GetEmployeeArbitrary(fmt.Sprintf("WHERE aoi LIKE %%s% AND prevTeam == 1 AND status == 0 AND speciality LIKE %%s%", project.AOIs[0].Aoi, project.Type))
 	var ret []Employee
 	for i := 0; i < len(project.AOIs); i++ {
-		people, err := GetEmployeeArbitrary(fmt.Sprintf("WHERE aoi LIKE %%s% AND status == 0 AND speciality LIKE %%s%", project.AOIs[0].Aoi, project.Type))
+		people, err := GetEmployeeArbitrary(fmt.Sprintf("WHERE aoi LIKE %%s% AND status == 0 AND speciality LIKE %%s%", project.AOIs[i].Aoi, project.Type))
 		if err != nil {
 			InfoLogger.Printf("You did something terribly wrong, %s\n", err)
 		}
