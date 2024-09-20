bc569aa0b9ef7bcb28e057ad3a11290326dff3c8
sturm1619 <carlosrubio1921@gmail.com>
2024-09-20 13:54:23 -0600

    Added ballons to file_upload

--- frontend/file_upload.py
+++ frontend/file_upload.py
@@ -59,6 +59,7 @@ def import_csv_data(*, testing=False):
         )
         if response.status_code == 200:
             st.toast("CSV uploaded succesfully", icon=":material/thumb_up:")
+            st.balloons()
             st.success(
                 "CSV uploaded successfully. Now you can check status or chat with the Assistant to build teams!", 
                 icon=":material/thumb_up:"
