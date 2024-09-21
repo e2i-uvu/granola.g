c3d8be89edaafa13afd0d278d0e0c2aae32afe48
sturm1619 <carlosrubio1921@gmail.com>
2024-09-20 20:31:03 -0600

    Added pdf parser team building

--- frontend/teams.py
+++ frontend/teams.py
@@ -1,7 +1,15 @@
 import streamlit as st
+from pypdf import PdfReader
 
 st.title("Team Import :material/reduce_capacity:")
 st.write("---")
+
+def read_pdf(pdf_file) -> str:
+    reader = PdfReader(pdf_file)
+    content: str = ""
+    for page in reader.pages:
+        content += page.extract_text()
+    return content
 
 files = st.file_uploader(
     "Project Charter",
@@ -11,4 +19,6 @@ files = st.file_uploader(
 
 
 if files is not None:
-    pass
+    for file in files:
+        # NOTE: project_description is a string containing text form pdf
+        project_description: str = read_pdf(file)
